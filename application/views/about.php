6fb0c4ebe742ba047a8a7d1b3796bc5d4901d79c
ivanardi07 <ardiyanto.ivan07@gmail.com>
2021-10-25 19:07:21 +0700

    first commit

---
+++ application/views/about.php
@@ -0,0 +1,37 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-1"></div>
+          <div class="col-sm-11">
+          	<h1 class="m-0">About</h1>
+          </div>
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-1"></div>
+          <div class="col-lg-2">
+          	<img src="<?= base_url('asset/ivan.jpg') ?>" width="100%">
+          </div>
+          <div class="col-lg-8">
+          	<p>Aplikasi ini dibuat oleh :</p>
+            <p>Name : Ivan Ardiyanto <br>
+            NIM : 1831710105 <br>
+            Tanggal : 25 Oktober 2021 </p>
+          </div>
+        </div>
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
